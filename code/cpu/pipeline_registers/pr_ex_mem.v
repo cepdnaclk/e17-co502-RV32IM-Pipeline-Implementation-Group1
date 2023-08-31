0fc6dd4fd7a7073b833d88dc320ead108371f7c0
Imesh <imesh.bl@gmail.com>
2023-08-31 20:20:26 +0530

    pipeline register fixes

--- code/cpu/pipeline_registers/pr_ex_mem.v
+++ code/cpu/pipeline_registers/pr_ex_mem.v
@@ -10,25 +10,25 @@ module pr_ex_mem (
     CLK, RESET, 
 
     EX_PC, EX_ALU_OUT, EX_REG_DATA2,
-    EX_REG_WRITE_ADDR, EX_REG_READ_ADDR2, EX_REG_WRITE_EN, 
+    EX_REG_WRITE_ADDR, EX_REG_WRITE_EN, 
     EX_DATA_MEM_WRITE, EX_DATA_MEM_READ, EX_WB_VALUE_SELECT,
 
     MEM_PC, MEM_ALU_OUT, MEM_REG_DATA2,
-    MEM_REG_WRITE_ADDR, MEM_REG_READ_ADDR2, MEM_REG_WRITE_EN,
+    MEM_REG_WRITE_ADDR, MEM_REG_WRITE_EN,
     MEM_DATA_MEM_WRITE, MEM_DATA_MEM_READ, MEM_WB_VALUE_SELECT
 );
 
     input CLK, RESET;
 
     input [31:0] EX_PC, EX_ALU_OUT, EX_REG_DATA2;
-    input [4:0] EX_REG_WRITE_ADDR, EX_REG_READ_ADDR2;
+    input [4:0] EX_REG_WRITE_ADDR;
     input EX_REG_WRITE_EN;
     input [3:0] EX_DATA_MEM_READ;
     input [2:0] EX_DATA_MEM_WRITE;
     input [1:0] EX_WB_VALUE_SELECT;
 
     output reg [31:0] MEM_PC, MEM_ALU_OUT, MEM_REG_DATA2;
-    output reg [4:0] MEM_REG_WRITE_ADDR, MEM_REG_READ_ADDR2;
+    output reg [4:0] MEM_REG_WRITE_ADDR;
     output reg MEM_REG_WRITE_EN;
     output reg [3:0] MEM_DATA_MEM_READ;
     output reg [2:0] MEM_DATA_MEM_WRITE;
@@ -39,15 +39,14 @@ module pr_ex_mem (
         
         if (RESET == 1'b1)
         begin
-            MEM_PC <= #0.1 32'b0;
-            MEM_ALU_OUT <= #0.1 32'b0;
-            MEM_REG_DATA2 <= #0.1 32'b0;
-            MEM_REG_WRITE_ADDR <= #0.1 4'b0;
-            MEM_REG_READ_ADDR2 <= #0.1 4'b0;
-            MEM_REG_WRITE_EN <= #0.1 1'b0;
-            MEM_DATA_MEM_WRITE <= #0.1 3'b0;
-            MEM_DATA_MEM_READ <= #0.1 4'b0;
-            MEM_WB_VALUE_SELECT <= #0.1 2'b0;
+            MEM_PC <= #0.1 32'd0;
+            MEM_ALU_OUT <= #0.1 32'd0;
+            MEM_REG_DATA2 <= #0.1 32'd0;
+            MEM_REG_WRITE_ADDR <= #0.1 4'd0;
+            MEM_REG_WRITE_EN <= #0.1 1'd0;
+            MEM_DATA_MEM_WRITE <= #0.1 3'd0;
+            MEM_DATA_MEM_READ <= #0.1 4'd0;
+            MEM_WB_VALUE_SELECT <= #0.1 2'd0;
         end
         else
         begin
@@ -55,7 +54,6 @@ module pr_ex_mem (
             MEM_ALU_OUT <= #0.1 EX_ALU_OUT;
             MEM_REG_DATA2 <= #0.1 EX_REG_DATA2;
             MEM_REG_WRITE_ADDR <= #0.1 EX_REG_WRITE_ADDR;
-            MEM_REG_READ_ADDR2 <= #0.1 EX_REG_READ_ADDR2;
             MEM_REG_WRITE_EN <= #0.1 EX_REG_WRITE_EN;
             MEM_DATA_MEM_WRITE <= #0.1 EX_DATA_MEM_WRITE;
             MEM_DATA_MEM_READ <= #0.1 EX_DATA_MEM_READ;
--- code/cpu/pipeline_registers/pr_id_ex.v
+++ code/cpu/pipeline_registers/pr_id_ex.v
@@ -10,13 +10,13 @@ module pr_id_ex (
     CLK, RESET, 
 
     ID_PC, ID_REG_DATA1, ID_REG_DATA2, ID_IMMEDIATE,
-    ID_REG_WRITE_ADDR, ID_REG_READ_ADDR1, ID_REG_READ_ADDR2, 
+    ID_REG_WRITE_ADDR,
     ID_ALU_SELECT, ID_OPERAND1_SELECT, ID_OPERAND2_SELECT,
     ID_REG_WRITE_EN, ID_DATA_MEM_WRITE, ID_DATA_MEM_READ, 
     ID_BRANCH_CTRL, ID_WB_VALUE_SELECT,
 
     EX_PC, EX_REG_DATA1, EX_REG_DATA2, EX_IMMEDIATE,
-    EX_REG_WRITE_ADDR, EX_REG_READ_ADDR1, EX_REG_READ_ADDR2,
+    EX_REG_WRITE_ADDR,
     EX_ALU_SELECT, EX_OPERAND1_SELECT, EX_OPERAND2_SELECT,
     EX_REG_WRITE_EN, EX_DATA_MEM_WRITE, EX_DATA_MEM_READ,
     EX_BRANCH_CTRL, EX_WB_VALUE_SELECT
@@ -25,7 +25,7 @@ module pr_id_ex (
     input CLK, RESET;
 
     input [31:0] ID_PC, ID_REG_DATA1, ID_REG_DATA2, ID_IMMEDIATE;
-    input [4:0] ID_REG_WRITE_ADDR, ID_REG_READ_ADDR1, ID_REG_READ_ADDR2;
+    input [4:0] ID_REG_WRITE_ADDR;
     input ID_REG_WRITE_EN, ID_OPERAND1_SELECT, ID_OPERAND2_SELECT;
     input [5:0] ID_ALU_SELECT;
     input [3:0] ID_DATA_MEM_READ, ID_BRANCH_CTRL;
@@ -33,7 +33,7 @@ module pr_id_ex (
     input [1:0] ID_WB_VALUE_SELECT;
 
     output reg [31:0] EX_PC, EX_REG_DATA1, EX_REG_DATA2, EX_IMMEDIATE;
-    output reg [4:0] EX_REG_WRITE_ADDR, EX_REG_READ_ADDR1, EX_REG_READ_ADDR2;
+    output reg [4:0] EX_REG_WRITE_ADDR;
     output reg EX_REG_WRITE_EN, EX_OPERAND1_SELECT, EX_OPERAND2_SELECT;
     output reg [5:0] EX_ALU_SELECT;
     output reg [3:0] EX_DATA_MEM_READ, EX_BRANCH_CTRL;
@@ -44,21 +44,19 @@ module pr_id_ex (
     begin
         if (RESET == 1'b1)
         begin
-            EX_PC <= #0.1 32'b0;
-            EX_REG_DATA1 <= #0.1 32'b0;
-            EX_REG_DATA2 <= #0.1 32'b0;
-            EX_IMMEDIATE <= #0.1 32'b0;
-            EX_REG_WRITE_ADDR <= #0.1 4'b0;
-            EX_REG_READ_ADDR1 <= #0.1 4'b0;
-            EX_REG_READ_ADDR2 <= #0.1 4'b0;
-            EX_ALU_SELECT <= #0.1 5'b0;
-            EX_OPERAND1_SELECT <= #0.1 1'b0;
-            EX_OPERAND2_SELECT <= #0.1 1'b0;
-            EX_REG_WRITE_EN <= #0.1 1'b0;
-            EX_DATA_MEM_WRITE <= #0.1 3'b0;
-            EX_DATA_MEM_READ <= #0.1 4'b0;
-            EX_BRANCH_CTRL <= #0.1 4'b0;
-            EX_WB_VALUE_SELECT <= #0.1 2'b0;
+            EX_PC <= #0.1 32'd0;
+            EX_REG_DATA1 <= #0.1 32'd0;
+            EX_REG_DATA2 <= #0.1 32'd0;
+            EX_IMMEDIATE <= #0.1 32'd0;
+            EX_REG_WRITE_ADDR <= #0.1 4'd0;
+            EX_ALU_SELECT <= #0.1 5'd0;
+            EX_OPERAND1_SELECT <= #0.1 1'd0;
+            EX_OPERAND2_SELECT <= #0.1 1'd0;
+            EX_REG_WRITE_EN <= #0.1 1'd0;
+            EX_DATA_MEM_WRITE <= #0.1 3'd0;
+            EX_DATA_MEM_READ <= #0.1 4'd0;
+            EX_BRANCH_CTRL <= #0.1 4'd0;
+            EX_WB_VALUE_SELECT <= #0.1 2'd0;
         end
         else
         begin
@@ -67,8 +65,6 @@ module pr_id_ex (
             EX_REG_DATA2 <= #0.1 ID_REG_DATA2;
             EX_IMMEDIATE <= #0.1 ID_IMMEDIATE;
             EX_REG_WRITE_ADDR <= #0.1 ID_REG_WRITE_ADDR;
-            EX_REG_READ_ADDR1 <= #0.1 ID_REG_READ_ADDR1;
-            EX_REG_READ_ADDR2 <= #0.1 ID_REG_READ_ADDR2;
             EX_ALU_SELECT <= #0.1 ID_ALU_SELECT;
             EX_OPERAND1_SELECT <= #0.1 ID_OPERAND1_SELECT;
             EX_OPERAND2_SELECT <= #0.1 ID_OPERAND2_SELECT;
